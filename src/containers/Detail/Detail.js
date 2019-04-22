d07ec2b571c8beb44c55becc18e681970efd62fd
liyong003 <liyong003@chinasofti.com>
2019-04-22 17:04:48 +0800

    修改详情页

--- src/containers/Detail/Detail.js
+++ src/containers/Detail/Detail.js
@@ -4,7 +4,7 @@ class Detail extends Component {
     render () {
         return (
             <div>
-                <span>这是detail页面</span>
+                <span>这是detail详情页面</span>
                 <span>
                     {this.props.match.params.id != null ? "，id为"+this.props.match.params.id:null}
                 </span>
